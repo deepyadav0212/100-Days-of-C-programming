28407d307b5a827354ea4239b58d58eb31f23bef
deepyadav0212 <deepyadav0212@gmail.com>
2025-10-28 11:52:28 +0530

    >

---
+++ Week1/Day1_Q1.c
@@ -0,0 +1,19 @@
+// write a program to input two numbers and display their sum
+
+#include <stdio.h>
+
+int main()
+{
+    int x, y, sum;
+    printf("Enter the first number:");
+    scanf("%d", &x);
+
+    printf("Enter the second number:");
+    scanf("%d", &y);
+
+    sum = x + y;
+
+    printf("The sum of %d and %d is %d\n", x, y, sum);
+
+    return 0;
+}
